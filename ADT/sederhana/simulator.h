e94e2261520befdd68c0b9e8bcedcad3306857cd
melvinkj <melvinkentj@gmail.com>
2022-10-30 16:19:37 +0700

    Adding properties to ADT Simulator

--- ADT/sederhana/simulator.h
+++ ADT/sederhana/simulator.h
@@ -1,5 +1,7 @@
 #include "../stack_queue/prioqueue.h"
+#include "../list_statik/liststatik.h"
 #include "point.h"
+#include "waktu.h"
 
 typedef struct{
     char * content; /* container character */
@@ -9,19 +11,28 @@ typedef struct{
 typedef struct simulator {
     string nama;
     POINT posisi;
+    TIME waktu;
     PrioQueue inventory;
+    PrioQueue delivery;
+    ListStatik kulkas;
 
 } Simulator;
 
 #define Nama(s) (s).nama
 #define Posisi(s) (s).posisi
+#define Waktu(s) (s).waktu
 #define Inventory(s) (s).inventory
+#define Delivery(s) (s).delivery
+#define Kulkas(s) (s).kulkas
 
-Simulator CreateSimulator(string nama, POINT posisi, PrioQueue inventory){
+Simulator CreateSimulator(string nama, POINT posisi, TIME waktu, PrioQueue inventory, PrioQueue delivery, ListStatik kulkas){
 /* Membentuk Simulator dari komponen-komponennya */
     Simulator s;
 
     Nama(s) = nama;
     Posisi(s) = posisi;
+    Waktu(s) = waktu;
     Inventory(s) = inventory;
+    Delivery(s) = delivery;
+    Kulkas(s) = kulkas;
 }
