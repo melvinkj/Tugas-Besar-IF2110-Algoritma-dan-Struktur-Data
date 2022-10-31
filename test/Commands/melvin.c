c9996f2d6064e6dbed189147aad1b5409bec34a9
Jimly-Firdaus <16521312@std.stei.itb.ac.id>
2022-10-31 09:49:28 +0700

    Pull cache

---
+++ test/Commands/melvin.c
@@ -0,0 +1,71 @@
+#include "../ADT/sederhana/point.h"
+#include "../ADT/sederhana/makanan.h"
+#include "../ADT/sederhana/waktu.h"
+#include "../ADT/sederhana/boolean.h"
+#include "../ADT/sederhana/simulator.h"
+#include "../ADT/matrix/matrix.h"
+
+int main(){
+    
+    Simulator sim;
+    Matrix peta;
+    string s;
+
+
+    CreateSimulator(sim, peta);
+
+    printf("BNMO di posisi: ");
+    TulisPOINT(Posisi(sim));
+    printf("Waktu:");
+    TulisTIME(Waktu(sim));
+    printf("Notifikasi: ");
+    // GIMANA INI
+
+
+    // FUNGSI NGEPRINT PETA BY YOBEL
+
+    printf("Enter Command: ");
+
+
+
+    if (x == "BUY") {
+
+    } else if (x == "FRY"){
+
+
+    } else if (x == "DELIVERY"){
+
+
+    } else if (x == "MOVE NORTH"){
+
+    } else if (x == "MOVE EAST"){
+
+    } else if (x == "MOVE WEST"){
+
+    } else if (x == "MOVE SOUTH"){
+
+    } else if (x == "MIX"){
+
+    } else if (x == "CHOP"){
+
+    } else if (x == "BOIL"){
+
+    } else if (x == "WAIT x y"){
+
+    } else if (x == "UNDO"){
+
+    } else if (x == "REDO"){
+
+    } else if (x == "CATALOG"){
+
+    } else if (x == "COOKBOOK"){
+
+    } else if(x == "INVALID"){
+
+    }
+    
+
+
+
+    return 0;
+}
