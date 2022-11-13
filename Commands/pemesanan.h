c69392d8a9ce6f758aa08f124155e4dec8c50f51
melvinkj <melvinkentj@gmail.com>
2022-11-13 16:07:29 +0700

    Fixing ADT Simulator

---
+++ Commands/pemesanan.h
@@ -0,0 +1,12 @@
+/* ADT Pengolahan makanan */
+
+#include <stdio.h>
+#include "../ADT/sederhana/makanan.h"
+#include "../ADT/sederhana/simulator.h"
+#include "../ADT/stack_queue/prioqueue.h"
+#include "../ADT/list_statik_resep/liststatikresep.h"
+#include "../ADT/listdin/listdin.h"
+#include "../ADT/list_statik/liststatik.h"
+
+
+void buy (ListMakanan LM, Simulator *s);
