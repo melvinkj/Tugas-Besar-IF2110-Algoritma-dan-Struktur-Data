8177b56276ce26a0443ea1034c8e3b7da3654284
manuellaiv <89376200+manuellaiv@users.noreply.github.com>
2022-11-18 18:51:07 +0700

    .

--- Commands/pengolahan.h
+++ Commands/pengolahan.h
@@ -8,6 +8,7 @@
 #include "../ADT/list_statik_resep/liststatikresep.h"
 #include "../ADT/listdin/listdin.h"
 #include "../ADT/list_statik/liststatik.h"
+#include "../command/inventory_delivery.h"
 
 #ifndef PENGOLAHAN_H
 #define PENGOLAHAN_H
